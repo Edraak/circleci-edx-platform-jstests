e55258ed7f5459dc3a7e00348a498a2541c6ff51
Omar Al-Ithawi <i@omardo.com>
2017-01-11 13:05:57 +0200

    Undo timeid stuff

--- scripts/circle-ci-tests.sh
+++ scripts/circle-ci-tests.sh
@@ -74,15 +74,15 @@ else
             ;;
 
         1)  # run all of the lms unit tests
-            paver test_system -s lms --with-flaky --cov-args="-p --timeid" --with-xunitmp
+            paver test_system -s lms --with-flaky --cov-args="-p" --with-xunitmp
             ;;
 
         2)  # run all of the cms unit tests
-            paver test_system -s cms --with-flaky --cov-args="-p --timeid" --with-xunitmp
+            paver test_system -s cms --with-flaky --cov-args="-p" --with-xunitmp
             ;;
 
         3)  # run the commonlib unit tests
-            paver test_lib --with-flaky --cov-args="-p --timeid" --with-xunitmp
+            paver test_lib --with-flaky --cov-args="-p" --with-xunitmp
             ;;
 
         *)
